6e6d31c5cc4881311ffb4134369922c2ad51d3d4
python490 <python490@gmail.com>
2015-10-08 13:52:01 -0700

    retrieve Facebook ID, and Token

--- app/src/main/java/com/example/android/vozie/MainActivity.java
+++ app/src/main/java/com/example/android/vozie/MainActivity.java
@@ -1,19 +1,64 @@
 package com.example.android.vozie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callBackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        callBackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
+        info = (TextView) findViewById(R.id.info);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n"
+                                + "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attemped failed");
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callBackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
