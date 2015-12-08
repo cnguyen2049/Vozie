fdfcfab8e16e4798c4f7ec2b2fefc1740158be64
python490 <python490@gmail.com>
2015-12-07 18:52:32 -0800

    Spinner values converted into proper Int values for Stripe

--- app/src/main/java/com/example/android/vozie/MainActivity.java
+++ app/src/main/java/com/example/android/vozie/MainActivity.java
@@ -7,17 +7,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 
-import com.facebook.FacebookSdk;
+import com.stripe.android.model.Card;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity{
         return super.onOptionsItemSelected(item);
     }
 
-    public String processText(View view){
+    public String processText(View view) {
 
         EditText cardNameField = (EditText) findViewById(R.id.card_name);
         String cardName = cardNameField.getText().toString();
@@ -52,9 +53,69 @@ public class MainActivity extends AppCompatActivity{
 
         EditText securityCodeField = (EditText) findViewById(R.id.security_code);
         String securityCode = securityCodeField.getText().toString();
-        String output = cardName + " " + cardNumber + " " + securityCode;
+
+        Spinner month_spinner = (Spinner) findViewById(R.id.month_spinner);
+        String month_string = month_spinner.getSelectedItem().toString();
+        Spinner year_spinner = (Spinner) findViewById(R.id.year_spinner);
+
+        String year_string = year_spinner.getSelectedItem().toString();
+        Integer year_int = Integer.parseInt(year_string);
+
+        Integer month_int = getIntMonthFromString(month_string);
+
+        if(month_int == 0){
+            return null;
+        }
+        String output = cardName + " " + cardNumber + " " + securityCode + " " + year_int +
+                " " + month_int;
         Log.v(LOG_TAG, output);
 
         return output;
+    }
+
+    public int getIntMonthFromString(String month) {
+        switch (month) {
+            case "Jan":
+                return 1;
+
+            case "Feb":
+                return 2;
+
+            case "Mar":
+                return 3;
+
+            case "Apr":
+                return 4;
+
+            case "May":
+                return 5;
+
+            case "Jun":
+                return 6;
+
+            case "Jul":
+                return 7;
+
+            case "Aug":
+                return 8;
+
+            case "Sep":
+                return 9;
+
+            case "Oct":
+                return 10;
+
+            case "Nov":
+                return 11;
+
+            case "Dec":
+                return 12;
+        }
+
+        return 0;
+    }
+
+    public void processCard(String number, int month, int year, String securityCode) {
+        Card card = new Card(number, month, year, securityCode);
     }
 }
