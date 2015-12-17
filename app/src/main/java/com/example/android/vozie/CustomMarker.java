dff37421e2c627a1318cde15e6889078e03d5a38
Shane Duffy <Dev.ShaneDuffy@Gmail.com>
2015-12-17 13:56:00 -0800

    Implemented Search Bar & Error Alert Class
    Search bar with autocompletion for all addresses within the United
    States. Error Alert class creates error dialogue for issue specified in
    calling parameter.
    
    Need to multithread the autocompletion feature as the address indexing
    creates lag whilst typing.

---
+++ app/src/main/java/com/example/android/vozie/CustomMarker.java
@@ -0,0 +1,8 @@
+package com.example.android.vozie;
+
+
+import android.support.v4.app.FragmentActivity;
+
+public class CustomMarker extends FragmentActivity {
+
+}
--- app/src/main/java/com/example/android/vozie/MainActivity.java
+++ app/src/main/java/com/example/android/vozie/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.vozie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.facebook.FacebookSdk;
 
@@ -14,6 +16,9 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        this.startActivity(intent);
     }
 
     @Override
