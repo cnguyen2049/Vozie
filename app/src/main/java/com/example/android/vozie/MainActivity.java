da6dfb24946d39221310e653c4f2a447f488d8ee
Shane Duffy <Dev.ShaneDuffy@Gmail.com>
2016-04-01 22:45:35 -0700

    Gradient Background Effect
    So I made this function earlier that created a nice animated gradient
    effect using the power of DrawingTransitional. Looked like it would be a
    cool background for like a login page or something.

--- app/src/main/java/com/example/android/vozie/MainActivity.java
+++ app/src/main/java/com/example/android/vozie/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.android.vozie;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -45,6 +51,15 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        ImageView image = (ImageView) findViewById(R.id.background);
+
+        Drawable backgrounds[] = new Drawable[4];
+        backgrounds[0] = ContextCompat.getDrawable(this, R.drawable.gradient1);
+        backgrounds[1] = ContextCompat.getDrawable(this, R.drawable.gradient2);
+        backgrounds[2] = ContextCompat.getDrawable(this, R.drawable.gradient3);
+        backgrounds[3] = ContextCompat.getDrawable(this, R.drawable.gradient4);
+
+        Crossfade(image, backgrounds, 10000);
     }
 
     public void login(){
@@ -68,7 +83,7 @@ public class MainActivity extends ActionBarActivity {
                                         String name = json.getString("name");
                                         String email = json.getString("email");
                                         String id = json.getString("id");
-                                        setText(id,name,email);
+                                        setText(id, name, email);
 
                                     } catch (JSONException e) {
                                         e.printStackTrace();
@@ -126,5 +141,82 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void Crossfade(final ImageView image, final Drawable layers[], final int speedInMs) {
+        class BackgroundGradientThread implements Runnable {
+            Context mainContext;
+            TransitionDrawable crossFader;
+            boolean first = true;
+
+            BackgroundGradientThread(Context c) {
+                mainContext = c;
+            }
+
+            public void run() {
+                Handler mHandler = new Handler(mainContext.getMainLooper());
+                boolean reverse = false;
+
+                while (true) {
+                    if (!reverse) {
+                        for (int i = 0; i < layers.length - 1; i++) {
+                            Drawable tLayers[] = new Drawable[2];
+                            tLayers[0] = layers[i];
+                            tLayers[1] = layers[i + 1];
+
+                            final TransitionDrawable tCrossFader = new TransitionDrawable(tLayers);
+                            tCrossFader.setCrossFadeEnabled(true);
+
+                            Runnable transitionRunnable = new Runnable() {
+                                @Override
+                                public void run() {
+                                    image.setImageDrawable(tCrossFader);
+                                    tCrossFader.startTransition(speedInMs);
+                                }
+                            };
+
+                            mHandler.post(transitionRunnable);
+
+                            try {
+                                Thread.sleep(speedInMs);
+                            } catch (Exception e) {
+                            }
+                        }
+
+                        reverse = true;
+                    }
+                    else if (reverse) {
+                        for (int i = layers.length - 1; i > 0; i--) {
+                            Drawable tLayers[] = new Drawable[2];
+                            tLayers[0] = layers[i];
+                            tLayers[1] = layers[i - 1];
+
+                            final TransitionDrawable tCrossFader = new TransitionDrawable(tLayers);
+                            tCrossFader.setCrossFadeEnabled(true);
+
+                            Runnable transitionRunnable = new Runnable() {
+                                @Override
+                                public void run() {
+                                    image.setImageDrawable(tCrossFader);
+                                    tCrossFader.startTransition(speedInMs);
+                                }
+                            };
+
+                            mHandler.post(transitionRunnable);
+
+                            try {
+                                Thread.sleep(speedInMs);
+                            } catch (Exception e) {
+                            }
+                        }
+
+                        reverse = false;
+                    }
+                }
+            }
+        }
+
+        Thread backgroundThread = new Thread(new BackgroundGradientThread(this));
+        backgroundThread.start();
     }
 }
