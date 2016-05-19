d3b414067b7dd5cca07fd9efc185711a0fda3990
Shane Duffy <Dev.ShaneDuffy@Gmail.com>
2016-05-19 16:10:53 -0700

    Global Titlebar
    For some reason there's a weird box in the top left corner... I'll have
    to look at this later, but I swear it didn't occur the other day?

--- app/src/main/java/com/example/android/vozie/MainActivity.java
+++ app/src/main/java/com/example/android/vozie/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.vozie;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 import com.facebook.FacebookSdk;
 
@@ -18,8 +20,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        initializeTitleBar();
         return true;
     }
 
@@ -36,5 +37,16 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void initializeTitleBar() {
+        final ViewGroup actionBarLayout =
+                (ViewGroup) getLayoutInflater().inflate(R.layout.titlebar_layout, null);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setCustomView(actionBarLayout);
     }
 }
