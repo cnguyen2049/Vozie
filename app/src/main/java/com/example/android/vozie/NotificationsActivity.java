68957002bef19d373d97e47dad966f88e2f3f1e2
Shane Duffy <Dev.ShaneDuffy@Gmail.com>
2016-05-19 15:27:32 -0700

    Fixed colors and removed flashing animation
    Still need to set listeners, but I can add those when all of the
    activities have been completed.

--- app/src/main/java/com/example/android/vozie/NotificationsActivity.java
+++ app/src/main/java/com/example/android/vozie/NotificationsActivity.java
@@ -3,10 +3,10 @@ package com.example.android.vozie;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -31,30 +32,24 @@ import java.util.List;
 
 
 public class NotificationsActivity extends Activity {
+    private final int TRIP_CONFIRMED = 1;
+    private final int TRIP_COMPLETED = 2;
+    private final int PAYMENT_ADDED = 3;
+    private final int PAYMENT_RECEIVED = 4;
+    private final int MILES_ADDED = 5;
+    private final int DRIVER_ARRIVED = 6;
+
     private ListView mListView;
     private ListViewAdapter mAdapter;
-    private Context mContext = this;
-    private Handler mHandler;
-    private List<Thread> flashThreads;
-    private List<TextView> threadID;
+    private List<NotificationsItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_notifications);
 
-    mHandler = new Handler();
     mListView = (ListView) findViewById(R.id.main_list);
-
-    List<NotificationsItem> items = new ArrayList<NotificationsItem>();
-    flashThreads = new ArrayList<Thread>();
-    threadID = new ArrayList<TextView>();
-
-    items.add(new NotificationsItem("Your trip from 3002 Ironside Ct, San Jose CA to 7576 Juniper Lane has concluded", 1));
-    items.add(new NotificationsItem("This is an alert lol", 2));
-    items.add(new NotificationsItem("text3", 3));
-    items.add(new NotificationsItem("text4", 4));
-    items.add(new NotificationsItem("text5", 5));
+    items = new ArrayList<NotificationsItem>();
 
     mAdapter = new ListViewAdapter(this, items);
     mListView.setAdapter(mAdapter);
@@ -102,15 +97,21 @@ public class NotificationsActivity extends Activity {
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
             Log.e("ListView", "onNothingSelected:");
-        }});
-    }
+        }
+    });
 
-    public void addNotification(List<NotificationsItem> list, NotificationsItem item, ListViewAdapter adapter) {
-        list.add(item);
 
-        adapter.size++;
-        mListView.setAdapter(null);
-        mListView.setAdapter(adapter);
+        // Test Notifications
+        addNotification(new NotificationsItem("Trip from 3002 Ironside Ct. to 2764 Rainview Drive" +
+                " has been confirmed. Your driver is en route!", "05/07", "6:45 PM", TRIP_CONFIRMED));
+        addNotification(new NotificationsItem("Trip from 3002 Ironside Ct. to 2764 Rainview Drive" +
+                " has been completed!", "05/09", "1:58 PM", TRIP_COMPLETED));
+        addNotification(new NotificationsItem("New credit card ending in 0764 added to payment methods.", "05/09", "3:45 PM", PAYMENT_ADDED));
+        addNotification(new NotificationsItem("Your payment of $50.99 has been received. Miles will be " +
+                "credited to your account shortly.", "05/10", "7:35 PM", PAYMENT_RECEIVED));
+        addNotification(new NotificationsItem("50 miles have been credited to your account!", "05/06", "2:37 PM", MILES_ADDED));
+        addNotification(new NotificationsItem("Your chauffeur has arrived! If you have specific instructions" +
+                " or need help, feel free to call or text them.", "05/10", "4:58 PM", DRIVER_ARRIVED));
     }
 
     @Override
@@ -135,24 +136,27 @@ public class NotificationsActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void addNotification(NotificationsItem item) {
+        items.add(item);
+
+        mAdapter.size++;
+        mListView.setAdapter(null);
+        mListView.setAdapter(mAdapter);
+    }
+
     public class NotificationsItem {
-        public String itemsText;
+        public String itemsText, time, date;
         public int itemsType;
 
-        public NotificationsItem(String text, int type) {
+        public NotificationsItem(String text,String time, String date, int type) {
             this.itemsText = text;
+            this.time = time;
+            this.date = date;
             this.itemsType = type;
         }
     }
 
     public class ListViewAdapter extends BaseSwipeAdapter {
-        private final int TRIP_CONFIRMED = 1;
-        private final int TRIP_COMPLETED = 2;
-        private final int PAYMENT_ADDED = 3;
-        private final int PAYMENT_RECEIVED = 4;
-        private final int SUB_ADDED = 5;
-        private final int DRIVER_ARRIVED = 6;
-
         private Context mContext;
         private List<NotificationsItem> notificationsItems;
         private int size;
@@ -171,46 +175,78 @@ public class NotificationsActivity extends Activity {
 
         @Override
         public View generateView(int position, ViewGroup parent) {
-            View v = LayoutInflater.from(mContext).inflate(R.layout.trip_confirmed_notification, parent, false);
+            final int WIDTH = 85; // In DP units
+            final int PADDING = 20;
+            final int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, WIDTH, getResources().getDisplayMetrics()); // Converted to pixels
+            final int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, PADDING, getResources().getDisplayMetrics());
+
+            View v = LayoutInflater.from(mContext).inflate(R.layout.notification, parent, false);
             LinearLayout topPanel = (LinearLayout) v.findViewById(getSwipeLayoutResourceId(position)).findViewById(R.id.top_panel);
-            TextView titleView = (TextView) v.findViewById(getSwipeLayoutResourceId(position)).findViewById(R.id.title);
+            LinearLayout bottomPanel = (LinearLayout) v.findViewById(getSwipeLayoutResourceId(position)).findViewById(R.id.bottom_panel);
+            TextView time = (TextView) v.findViewById(getSwipeLayoutResourceId(position)).findViewById(R.id.time);
+            TextView date = (TextView) v.findViewById(getSwipeLayoutResourceId(position)).findViewById(R.id.date);
+            TextView titleTxt = (TextView) v.findViewById(getSwipeLayoutResourceId(position)).findViewById(R.id.title);
+
+            time.setText(notificationsItems.get(position).time);
+            date.setText(notificationsItems.get(position).date);
 
             switch (notificationsItems.get(position).itemsType) {
                 case TRIP_CONFIRMED:
                     title = "Trip Confirmed";
-                    setLayoutColor(topPanel, Color.parseColor("#FFB9604F"));
-                    flashComponent(titleView);
+                    topPanel.setBackgroundColor(Color.parseColor("#FFB9604F"));
+
+                    ImageView tripButton = new ImageView(parent.getContext());
+                    addSwipeButton(bottomPanel, tripButton, R.drawable.trip, padding, width, Color.parseColor("#71dbb8"));
                     break;
                 case TRIP_COMPLETED:
-                    title = "Journey Completed";
-                    setLayoutColor(topPanel, Color.parseColor("#FFC693A7"));
+                    title = "Trip Completed";
+                    topPanel.setBackgroundColor(Color.parseColor("#FFC693A7"));
+
+                    ImageView historyButton = new ImageView(parent.getContext());
+                    addSwipeButton(bottomPanel, historyButton, R.drawable.history, padding, width, Color.parseColor("#ffa152"));
+                    ImageView facebookButton = new ImageView(parent.getContext());
+                    addSwipeButton(bottomPanel, facebookButton, R.drawable.facebook, padding, width, Color.parseColor("#ADD8E6"));
                     break;
                 case PAYMENT_ADDED:
                     title = "New Payment Method";
-                    setLayoutColor(topPanel, Color.parseColor("#FFD06764"));
+                    topPanel.setBackgroundColor(Color.parseColor("#FFD06764"));
+
+                    ImageView paymentButton = new ImageView(parent.getContext());
+                    addSwipeButton(bottomPanel, paymentButton, R.drawable.payment, padding, width, Color.parseColor("#83D489"));
                     break;
                 case PAYMENT_RECEIVED:
                     title = "Payment Received";
-                    setLayoutColor(topPanel, Color.parseColor("#FFD0754F"));
+                    topPanel.setBackgroundColor(Color.parseColor("#FFD0754F"));
                     break;
-                case SUB_ADDED:
-                    title = "New Subscription Added";
-                    setLayoutColor(topPanel, Color.parseColor("#FFD0546E"));
+                case MILES_ADDED:
+                    title = "Miles Credited To Account";
+                    topPanel.setBackgroundColor(Color.parseColor("#FFD0546E"));
+
+                    ImageView milesButton = new ImageView(parent.getContext());
+                    addSwipeButton(bottomPanel, milesButton, R.drawable.miles, padding, width, Color.parseColor("#e4a6f0"));
                     break;
                 case DRIVER_ARRIVED:
-                    title = "Your Driver Has Arrived!";
-                    setLayoutColor(topPanel, Color.parseColor("#FFD06F66"));
+                    title = "Driver Has Arrived";
+                    topPanel.setBackgroundColor(Color.parseColor("#FFD06F66"));
+
+                    ImageView phoneButton = new ImageView(parent.getContext());
+                    addSwipeButton(bottomPanel, phoneButton, R.drawable.phone, padding, width, Color.parseColor("#7198db"));
+                    ImageView textButton = new ImageView(parent.getContext());
+                    addSwipeButton(bottomPanel, textButton, R.drawable.text, padding, width, Color.parseColor("#f0848f"));
                     break;
                 default:
                     title = "Notification";
                     break;
             }
 
+            ImageView deleteButton = new ImageView(parent.getContext());
+            addSwipeButton(bottomPanel, deleteButton, R.drawable.delete, padding, width, Color.parseColor("#DFDFDF"));
+
             SwipeLayout swipeLayout = (SwipeLayout)v.findViewById(getSwipeLayoutResourceId(position));
             swipeLayout.addSwipeListener(new SimpleSwipeListener() {
                 @Override
                 public void onOpen(SwipeLayout layout) {
-                    YoYo.with(Techniques.Pulse).duration(500).delay(100).playOn(layout.findViewById(R.id.trip));
+                    //YoYo.with(Techniques.Pulse).duration(500).delay(100).playOn(layout.findViewById(R.id.));
                 }
             });
             swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
@@ -219,48 +255,15 @@ public class NotificationsActivity extends Activity {
 
                 }
             });
-            v.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+            /*v.findViewById(R.id.delete_image).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
                 }
-            });
+            });*/
             return v;
         }
 
-        public void flashComponent(final TextView v) {
-            final int MAX_RADIUS = 4;
-            final int INTERVAL = 500;
-
-            flashThreads.add(new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (true) {
-                        runOnUiThread(new Runnable() {
-                            public void run() {
-                                if (v.getTag() == null)
-                                    v.setTag(true);
-
-                                float rad = v.getShadowRadius();
-                                boolean tag = (boolean) v.getTag();
-
-                                if (tag)
-                                    v.setShadowLayer(rad + 1, 2, 2, Color.parseColor("#FFFFFF"));
-                                else if (!tag)
-                                    v.setShadowLayer(rad - 1, 2, 2, Color.parseColor("#FFFFFF"));
-                                if (rad == MAX_RADIUS)
-                                    v.setTag(false);
-                                else if (rad == 1)
-                                    v.setTag(true);
-                            }
-                        });
-                        try { Thread.currentThread().sleep(INTERVAL); } catch (Exception e) {}
-                    }
-                }
-            }));
-
-            flashThreads.get(0).start();
-        }
         @Override
         public void fillValues(int position, View convertView) {
             TextView t2 = (TextView)convertView.findViewById(R.id.title);
@@ -285,12 +288,13 @@ public class NotificationsActivity extends Activity {
             return position;
         }
 
-        public void setLayoutColor(final LinearLayout l, final int c) {
-            mHandler.post(new Runnable() {
-                public void run() {
-                    l.setBackgroundColor(c);
-                }
-            });
+        public void addSwipeButton(LinearLayout panel, ImageView image, int resource, int padding, int width, int background) {
+            image.setImageResource(resource);
+            image.setPadding(padding, padding, padding, padding);
+            image.setLayoutParams(new ViewGroup.LayoutParams(width, ViewGroup.LayoutParams.MATCH_PARENT));
+            image.setBackgroundColor(background);
+
+            panel.addView(image);
         }
     }
 }
